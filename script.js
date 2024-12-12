2ea1238e278d2788b678611837b38349ff83d400
mohamedshawky05 <mohamedshawky0981@gmail.com>
2024-12-12 06:22:23 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,75 @@
+const apiKey = "9c35055661c797c68a734369f5eb8cab"; 
+const searchBtn = document.getElementById("search-btn");
+const cityInput = document.getElementById("city-input");
+
+const cityName = document.getElementById("city-name");
+const temperature = document.getElementById("temperature");
+const description = document.getElementById("description");
+const weatherIcon = document.getElementById("weather-icon");
+
+searchBtn.addEventListener("click", () => {
+  const city = cityInput.value.trim();
+  if (city) {
+    fetchWeather(city);
+  } else {
+    alert("Please enter a city name!");
+  }
+});
+
+function fetchWeather(city) {
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+  fetch(url)
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error("City not found");
+      }
+      return response.json();
+    })
+    .then((data) => {
+      displayWeather(data);
+    })
+    .catch((error) => {
+      alert(error.message);
+    });
+}
+
+function displayWeather(data) {
+  cityName.textContent = data.name;
+  temperature.textContent = `Temperature: ${data.main.temp}°C`;
+  description.textContent = `Weather: ${data.weather[0].description}`;
+  weatherIcon.src = `https://openweathermap.org/img/wn/${data.weather[0].icon}.png`;
+}
+
+	if ('WebSocket' in window) {
+		(function () {
+			function refreshCSS() {
+				var sheets = [].slice.call(document.getElementsByTagName("link"));
+				var head = document.getElementsByTagName("head")[0];
+				for (var i = 0; i < sheets.length; ++i) {
+					var elem = sheets[i];
+					var parent = elem.parentElement || head;
+					parent.removeChild(elem);
+					var rel = elem.rel;
+					if (elem.href && typeof rel != "string" || rel.length == 0 || rel.toLowerCase() == "stylesheet") {
+						var url = elem.href.replace(/(&|\?)_cacheOverride=\d+/, '');
+						elem.href = url + (url.indexOf('?') >= 0 ? '&' : '?') + '_cacheOverride=' + (new Date().valueOf());
+					}
+					parent.appendChild(elem);
+				}
+			}
+			var protocol = window.location.protocol === 'http:' ? 'ws://' : 'wss://';
+			var address = protocol + window.location.host + window.location.pathname + '/ws';
+			var socket = new WebSocket(address);
+			socket.onmessage = function (msg) {
+				if (msg.data == 'reload') window.location.reload();
+				else if (msg.data == 'refreshcss') refreshCSS();
+			};
+			if (sessionStorage && !sessionStorage.getItem('IsThisFirstTime_Log_From_LiveServer')) {
+				console.log('Live reload enabled.');
+				sessionStorage.setItem('IsThisFirstTime_Log_From_LiveServer', true);
+			}
+		})();
+	}
+	else {
+		console.error('Upgrade your browser. This Browser is NOT supported WebSocket for Live-Reloading.');
+	}
